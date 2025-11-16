276dbd6860b5c6868b6d44d3fe4797da090b3296
ShadyLight <Caseybx5@gmail.com>
2025-11-16 11:44:45 -0700

    Updated SystemSettingsSubmenu

--- Part 3/src/SystemSettingsSubmenu.java
+++ Part 3/src/SystemSettingsSubmenu.java
@@ -1,7 +1,4 @@
-
 import java.util.Scanner;
-
-
 /**
  *
  * @author Alexander
@@ -12,19 +9,20 @@ public class SystemSettingsSubmenu implements ProgramMenu{
     private Menu menu;
     private Inventory inventory;
     private CostCalculation costCalculation;
+    private Scanner scanner;
     
-    public SystemSettingsSubmenu(Restaurant restaurant, Menu menu, Inventory inventory, CostCalculation costCalculation){
+    public SystemSettingsSubmenu(Restaurant restaurant, Menu menu, Inventory inventory, CostCalculation costCalculation, Scanner scanner){
         this.restaurant = restaurant;
         this.menu = menu;
         this.inventory = inventory;
         this.costCalculation = costCalculation;
+        this.scanner = scanner;
     }
     
     @Override
     public void showMenu(){
         byte choice = 0;
-        Scanner input = new Scanner(System.in);
-        do{
+        while(true){
             System.out.print("Food Delivery Management System Settings\n"+
                                "______________________________________________\n"+
                                "1. Manage Restaurant Information\n"+
@@ -35,32 +33,29 @@ public class SystemSettingsSubmenu implements ProgramMenu{
                                "______________________________________________\n"+
                                "Please select your choice: ");
             
-            choice = input.nextByte();
-            
+            choice = scanner.nextByte();
+            scanner.nextLine();
             System.out.println("");
             
             switch(choice){
                 case 1:
-                    
+                    new RestaurantManagementMenu(restaurant, scanner).showMenu();
                     break;
                 case 2:
-                    
+                    new MenuManagementMenu(menu, scanner).showMenu();
                     break;
                 case 3:
-                    
+                    new InventoryManagementMenu(inventory, scanner).showMenu();
                     break;
                 case 4:
-                    
+                    new CostCalculationMenu(costCalculation, scanner).showMenu();
                     break;
                 case 5:
-                    
-                    break;
+                    return;
                 default:
-                    System.out.println("That choice was invalid..."+
-                                       "Please try again.");
+                    System.out.println("That choice was invalid..." + "Please try again.");
                     break;
             }
         }
-        while(choice != 5);
     }
 }
